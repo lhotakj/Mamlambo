54ba9f68023bea2d988ce1123fc814c102e433e2
lhotakj <lhotakj@gmail.com>
2019-08-06 22:08:23 +0200

    tests

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run apache on port 89
-sudo docker run -it --rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 89:80 --tmpfs /run --tmpfs /run/lock --name=apache lhotakj/centos-apache-mod_wsgi-python3:3.7.4
+sudo docker run -idt --rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 89:80 --tmpfs /run --tmpfs /run/lock --name=apache lhotakj/centos-apache-mod_wsgi-python3:3.7.4
 
 sudo docker cp ./demo apache:/tmp ...
 
